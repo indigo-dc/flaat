e2e7a1dfac801c6bf751f36e1e6d8d173fe5d6ae
Marcus <marcus@hardt-it.de>
2019-02-08 12:04:19 +0100

    add note about caching; shorten MIT License statement

--- flaat/issuertools.py
+++ flaat/issuertools.py
@@ -1,25 +1,5 @@
 '''Tools for token handling in FLAAT'''
-# MIT License{{{
-#
-# Copyright (c) 2017 - 2019 Karlsruhe Institute of Technology - Steinbuch Centre for Computing
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.}}}
+# This code is distributed under the MIT License
 # pylint # {{{
 # vim: tw=100 foldmethod=marker
 # pylint: disable=bad-continuation, invalid-name, superfluous-parens
@@ -107,7 +87,11 @@ def find_issuer_config_in_file(op_file, op_hint = None):# {{{
 # }}}
 
 def get_iss_config_from_endpoint(issuer_url):# {{{
-    '''Get issuer_wellknown/configuration from url; return json if true, None otherwise'''
+    '''Get issuer_wellknown/configuration from url; return json if true, None otherwise. 
+    Note that this endpoint is called more often than necessary. We rely on requests_cache to keep
+    this efficient'''
+    # If requests_cache is not wanted. Here would be one place to implement caching
+
     headers = {'Content-type': 'application/x-www-form-urlencoded'}
     config_url = issuer_url
     # remove slashes:
