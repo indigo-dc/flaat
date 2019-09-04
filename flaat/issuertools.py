5ace3e6c793a3d92a5408fbc9695402cebf84abb
Marcus <marcus@hardt-it.de>
2019-09-04 19:00:55 +0200

    add thread pooled search for user info; also add a timeout

--- flaat/issuertools.py
+++ flaat/issuertools.py
@@ -5,7 +5,15 @@
 # pylint: disable=bad-continuation, invalid-name, superfluous-parens
 # pylint: disable=bad-whitespace
 # pylint: disable=logging-not-lazy, logging-format-interpolation
+# pylint: disable=wrong-import-position
 
+import sys
+is_py2 = sys.version[0] == '2'
+if is_py2:
+    from Queue import Queue, Empty
+else:
+    from queue import Queue, Empty
+from threading import Thread
 import re
 import fileinput
 from base64 import b64encode
@@ -20,6 +28,10 @@ requests_cache.install_cache(include_get_headers=True, expire_after=300)
 
 verbose = 0
 verify_tls = True
+timeout = 4.2 #(seconds)
+num_request_workers = 10
+param_q  = Queue(num_request_workers*2)
+result_q = Queue(num_request_workers*2)
 
 def find_issuer_config_in_at(access_token):
     '''If there is an issuer in the AT, we fetch the ISS config and return it'''
@@ -51,10 +63,27 @@ def find_issuer_config_in_string(string):
             iss_config = get_iss_config_from_endpoint(string+'/oauth2'+'/.well-known/openid-configuration')
     return iss_config
 
-def find_issuer_config_in_list(op_list, op_hint = None, exclude_list = []):
+def thread_worker_issuerconfig():
+    '''Thread worker'''
+    while True:
+        item = param_q.get()
+        if item is None:
+            break
+        result = get_iss_config_from_endpoint(item)
+        result_q.put(result)
+        param_q.task_done()
+        result_q.task_done()
 
+def find_issuer_config_in_list(op_list, op_hint = None, exclude_list = []):
     '''find the hinted issuer in configured op_list'''
+
     iss_config = None
+    for i in range (num_request_workers):
+        t = Thread(target=thread_worker_issuerconfig)
+        # t = Thread(target=worker)
+        t.daemon = True
+        t.start()
+
     if op_list:
         iss_config=[]
         for issuer in op_list:
@@ -62,20 +91,30 @@ def find_issuer_config_in_list(op_list, op_hint = None, exclude_list = []):
                 if verbose>1:
                     print ('skipping %s due to exclude list' % issuer)
                 continue
+            issuer_wellknown=issuer.rstrip('/') + '/.well-known/openid-configuration'
             if op_hint is None:
-                issuer_wellknown=issuer.rstrip('/') + '/.well-known/openid-configuration'
-                iss_config.append(get_iss_config_from_endpoint(issuer_wellknown))
+                # print ('getting issuer config from {}'.format(issuer))
+                # iss_config.append(get_iss_config_from_endpoint(issuer_wellknown))
+                param_q.put(issuer_wellknown)
             else:
                 if re.search(op_hint, issuer):
-                    issuer_wellknown=issuer.rstrip('/') + '/.well-known/openid-configuration'
-                    iss_config.append(get_iss_config_from_endpoint(issuer_wellknown))
+                    # iss_config.append(get_iss_config_from_endpoint(issuer_wellknown))
+                    param_q.put(issuer_wellknown)
+        param_q.join()
+        result_q.join()
+        try:
+            for entry in iter(result_q.get_nowait, None):
+                iss_config.append(entry)
+        except Empty:
+            pass
+
     return iss_config
 
 def find_issuer_config_in_file(op_file, op_hint = None, exclude_list=[]):
-
     '''find the hinted issuer in a configured, oidc-agent compatible issuers.conf file
     we only use the first (space separated) entry of that file.'''
     iss_config = None
+    op_list = []
     if op_file:
         iss_config=[]
         for issuer in fileinput.input(op_file):
@@ -86,18 +125,12 @@ def find_issuer_config_in_file(op_file, op_hint = None, exclude_list=[]):
                 if verbose>1:
                     print ('skipping %s due to exclude list' % issuer)
                 continue
-            if op_hint is None:
-                issuer_wellknown=issuer_from_conf.rstrip('/') + '/.well-known/openid-configuration'
-                iss_config.append(get_iss_config_from_endpoint(issuer_wellknown))
-            else:
-                if re.search(op_hint, issuer):
-                    issuer_wellknown=issuer_from_conf.rstrip('/') + '/.well-known/openid-configuration'
-                    iss_config.append(get_iss_config_from_endpoint(issuer_wellknown))
+            op_list.append(issuer_from_conf)
+        return (find_issuer_config_in_list(op_list, op_hint, exclude_list))
     return iss_config
 
-
 def get_iss_config_from_endpoint(issuer_url):
-    '''Get issuer_wellknown/configuration from url; return json if true, None otherwise. 
+    '''Get issuer_wellknown/configuration from url; return json if true, None otherwise.
     Note that this endpoint is called more often than necessary. We rely on requests_cache to keep
     this efficient'''
     # If requests_cache is not wanted. Here would be one place to implement caching
@@ -112,7 +145,7 @@ def get_iss_config_from_endpoint(issuer_url):
 
     if verbose>1:
         print('Getting config from: %s' % config_url)
-    resp = requests.get (config_url, verify=verify_tls, headers=headers)
+    resp = requests.get (config_url, verify=verify_tls, headers=headers, timeout=timeout)
     if verbose > 2:
         print('Getconfig: resp: %s' % resp.status_code)
     try:
@@ -130,7 +163,8 @@ def get_user_info(access_token, issuer_config):
         print ('using this access token: %s' % access_token)
     if verbose > 1:
         print('Getting userinfo from %s' % issuer_config['userinfo_endpoint'])
-    resp = requests.get (issuer_config['userinfo_endpoint'], verify=verify_tls, headers=headers)
+    resp = requests.get (issuer_config['userinfo_endpoint'], verify=verify_tls, headers=headers,
+            timeout=timeout)
     if resp.status_code != 200:
         if verbose > 2:
             print('userinfo: Error: %s' % resp.status_code)
@@ -172,7 +206,7 @@ def get_introspected_token_info(access_token, issuer_config, client_id=None, cli
         print('Getting introspection from %s' % issuer_config['userinfo_endpoint'])
     try:
         resp = requests.post (issuer_config['introspection_endpoint'], \
-                              verify=verify_tls, headers=headers, data=post_data)
+                              verify=verify_tls, headers=headers, data=post_data, timeout=timeout)
     except KeyError: # no introspection_endpoint found
         return None
 
