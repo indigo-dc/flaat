af49245e2996a51ae9b0da8f6070412f974a102b
Marcus <marcus@hardt-it.de>
2020-08-07 13:47:15 +0200

    remove anything that looks like a dependency on flask

--- example-aio.py
+++ example-aio.py
@@ -88,7 +88,7 @@ def my_failure_callback(message='', *args, **kwargs):
     return 'User define failure callback.\nError Message: "%s"' % message
 
 @routes.get('/')
-async def root(request):
+async def root():
     text = '''This is an example for useing flaat with AIO. These endpoints are available:
     /info               General info about the access_token (if provided)
     /valid_user         Requires a valid user
@@ -124,12 +124,12 @@ async def info(any_argument):
 
 @routes.get('/valid_user')
 @flaat.login_required()
-async def valid_user(request):
+async def valid_user():
     return web.Response(text='This worked: there was a valid login')
 
 @routes.get('/valid_user_2')
 @flaat.login_required(on_failure=my_failure_callback)
-async def valid_user_own_callback(request):
+async def valid_user_own_callback():
     return web.Response(text='This worked: there was a valid login')
 
 
@@ -166,32 +166,32 @@ async def valid_user_pos_kw_args(*args, **kwargs):
 @flaat.group_required(group=['admins@kit.edu', 'employee@kit.edu', 'member@kit.edu'],
         claim='eduperson_scoped_affiliation', match=2,
         on_failure=my_failure_callback)
-async def demo_groups_kit(request):
+async def demo_groups_kit():
     return web.Response(text='This worked: user is member of the requested group')
 
 @routes.get('/group_test_iam')
 @flaat.group_required(group='KIT-Cloud', claim='groups')
-async def demo_groups_iam(request):
+async def demo_groups_iam():
     return web.Response(text='This worked: user is member of the requested group')
 
 @routes.get('/group_test_hdf')
 @flaat.aarc_g002_group_required(group=['urn:geant:h-df.de:group:aai-admin',
         'urn:geant:h-df.de:group:myExampleColab#unity.helmholtz-data-federation.de'],
         claim='eduperson_entitlement', match='all')
-async def demo_groups_hdf(request):
+async def demo_groups_hdf():
     return web.Response(text='This worked: user has the required entitlement(s)')
 
 @routes.get('/group_test_hdf2')
 @flaat.aarc_g002_group_required(group=[
         'urn:geant:h-df.de:group:myExampleColab#unity.helmholtz-data-federation.de'],
         claim='eduperson_entitlement', match='all')
-async def demo_groups_hdf2(request):
+async def demo_groups_hdf2():
     return web.Response(text='This worked: user has the required entitlement(s)')
 
 @routes.get('/group_test_hdf3')
 @flaat.aarc_g002_group_required(group=['urn:geant:h-df.de:group:aai-admin'],
         claim='eduperson_entitlement', match='all')
-async def demo_groups_hdf3(request):
+async def demo_groups_hdf3():
     return web.Response(text='This worked: user has the required entitlement(s)')
 
 app.add_routes(routes)
