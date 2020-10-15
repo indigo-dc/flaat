2c2a279480a1606a2bbe9ab0a4261f8cc64973be
Marcus <marcus@hardt-it.de>
2020-10-15 16:33:23 +0200

    move import after sys.path.insert

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,16 +19,18 @@
 import os
 import sys
 
-import flaat
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../flaat/'))
 sys.path.insert(0, os.path.abspath('../../flaat/'))
+sys.path.insert(0, os.path.abspath('../../../flaat/'))
+sys.path.insert(0, os.path.abspath('../../../'))
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('./'))
+
+import flaat
 
 # -- General configuration ----------------------------------------------------
 
