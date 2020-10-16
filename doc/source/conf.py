90e23cb96c8113f5df68fb56191bc886cadb8604
Marcus <marcus@hardt-it.de>
2020-10-16 07:45:36 +0200

    shrink path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,8 +24,8 @@ import sys
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('./'))
+# sys.path.insert(0, os.path.abspath('../'))
+# sys.path.insert(0, os.path.abspath('./'))
 
 import flaat
 
