4387a991c594dd4647d20c9c81fa519eff6550ef
Diana Gudu <diana.gudu@kit.edu>
2022-03-11 15:32:38 +0100

    jwt signature verification: search signing keys by type

--- flaat/access_tokens.py
+++ flaat/access_tokens.py
@@ -57,8 +57,55 @@ class AccessTokenInfo:
         return self.body.get("iss", "")
 
 
+def _get_signing_key_from_jwt(jwks_uri, access_token):
+    jwk_client = PyJWKClient(jwks_uri)
+
+    try:
+        return jwk_client.get_signing_key_from_jwt(access_token).key
+    except jwt.exceptions.PyJWKClientError:
+        # keys don't have 'kid' field, retrieve key by type, given the 'alg' in token header
+        unverified = jwt.api_jwt.decode_complete(
+            access_token, options={"verify_signature": False}
+        )
+        alg = unverified["header"][
+            "alg"
+        ]  # MUST be present, possible values defined at https://datatracker.ietf.org/doc/html/rfc7518#section-3.1
+
+        # algorithm is none, then signing key is None; signature must be empty octet string
+        if alg == "none":
+            return None
+        # infer key type from algorithm
+        key_type = ""
+        if alg.startswith("RS") or alg.startswith("PS"):
+            key_type = "RSA"
+        if alg.startswith("HS"):
+            key_type = "oct"
+        if alg.startswith("ES"):
+            key_type = "EC"
+        if alg.startswith("Ed"):
+            key_type = "OKP"
+        # get key from JWKS endpoint by key type
+        jwk_set = jwk_client.get_jwk_set()
+        signing_keys = [
+            jwk_set_key
+            for jwk_set_key in jwk_set.keys
+            if jwk_set_key.key_type == key_type
+        ]
+        if len(signing_keys) == 0:
+            raise FlaatUnauthenticated(
+                "Could not verify JWT: The JWKS endpoint did not contain any signing key "
+                f"of type {key_type}."
+            )
+        if len(signing_keys) > 1:
+            raise FlaatUnauthenticated(
+                f"Could not verify JWT: The JWKS endpoint has too many keys of type {key_type} "
+                "and no 'kid' specified."
+            )
+        return signing_keys[0].key
+
+
 def get_access_token_info(access_token, verify=True) -> Optional[AccessTokenInfo]:
-    unverified = None
+    unverified = {}
     try:
         unverified = jwt.api_jwt.decode_complete(
             access_token,
@@ -71,7 +118,7 @@ def get_access_token_info(access_token, verify=True) -> Optional[AccessTokenInfo
         return None
 
     if not verify:
-        return AccessTokenInfo(unverified_body, verification=None)
+        return AccessTokenInfo(unverified, verification=None)
 
     issuer = IssuerConfig.get_from_string(unverified_body.get("iss", ""))
     if issuer is None:
@@ -83,14 +130,17 @@ def get_access_token_info(access_token, verify=True) -> Optional[AccessTokenInfo
             "Could not verify JWT: Issuer config has no jwks_uri"
         )
 
-    jwk_client = PyJWKClient(jwks_uri)
-    signing_key = jwk_client.get_signing_key_from_jwt(access_token)
+    signing_key = _get_signing_key_from_jwt(jwks_uri, access_token)
+
     try:
         complete_decode = jwt.api_jwt.decode_complete(
-            access_token, signing_key.key, algorithms=PERMITTED_SIGNATURE_ALGORITHMS
+            access_token,
+            signing_key,
+            algorithms=PERMITTED_SIGNATURE_ALGORITHMS,
+            options={"verify_aud": False},
         )
-    except jwt.InvalidSignatureError as e:
-        raise FlaatUnauthenticated("Could not verify JWT: Invalid signature") from e
+    except jwt.exceptions.PyJWTError as e:
+        raise FlaatUnauthenticated(f"Could not verify JWT: {e}") from e
 
     return AccessTokenInfo(
         complete_decode,
