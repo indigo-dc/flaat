4f41e5852cb5acf7b820c963bfc98491ae0ac73c
BorjaEst <boressan@outlook.com>
2022-04-14 09:45:34 +0200

    Simplify tests names

--- flaat/flask/flask_test.py
+++ flaat/flask/flask_test.py
@@ -3,12 +3,12 @@ import flaat.flask.flask_test_cases as cases
 
 
 @parametrize_with_cases("path, headers", cases=cases.Authorized)
-def test_is_authorized(client, path, headers):
+def test_authorized(client, path, headers):
     response = client.get(path, headers=headers)
     assert response.status_code == 200
 
 
 @parametrize_with_cases("path, headers", cases=cases.Unauthorized)
-def test_not_authorized(client, path, headers):
+def test_unauthorized(client, path, headers):
     response = client.get(path, headers=headers)
     assert response.status_code == 401
