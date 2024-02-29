95c84ddff6c899ee09e2819773cc7e46976d6df5
Marcus <marcus@hardt-it.de>
2024-02-29 09:55:54 +0100

    ipv4 has four elements

--- flaat/issuers.py
+++ flaat/issuers.py
@@ -25,7 +25,7 @@ def is_url(string):
         r"^(?:http|ftp)s?://"  # http:// or https://
         r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?)(?:\.[A-Z]{2,6}|[A-Z0-9-]{2,})*\.?|\d{1,3}\."  # domain...
         r"localhost|"  # localhost...
-        r"\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ipv4
         r"(?::\d+)?"  # optional port
         r"(?:/?|[/?]\S+)$",
         re.IGNORECASE,
